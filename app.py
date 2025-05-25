97856a84cc7252af4e845c3af17dcc25d5b1c67e
Salman <salmaanmohamed700@gmail.com>
2025-05-25 18:05:04 +0300

    added app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///freebie_tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Company, Dev, Freebie
+
+if __name__ == '__main__':
+    app.run(debug=True)
